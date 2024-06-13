9372a4c0e508f75f02acdfa34e7473eca45bdefb
Samay Jain <samayjain666@gmail.com>
2024-06-13 17:51:36 +0530

    created mongoose schema models and routes

--- backend/index.js
+++ backend/index.js
@@ -7,9 +7,23 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello Samay!')
-})
+//Available Routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/notes', require('./routes/notes'));
+
+
+
+// app.get('/', (req, res) => {
+//   res.send('Hello Samay!')
+// })
+
+// app.get('/api/v1/login', (req, res) => {
+//     res.send('Hello Login!')
+// })
+
+// app.get('/api/v1/signup', (req, res) => {
+//     res.send('Hello Signup!')
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
