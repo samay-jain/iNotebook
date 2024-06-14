d669cc70da0dbe4cc67058cc04f47577134da82e
Samay Jain <samayjain666@gmail.com>
2024-06-14 17:20:22 +0530

    storing data into database using mongoose model

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 
-const mongoURI = 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false';
-
+//const mongoURI = 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false';
+const mongoURI = 'mongodb://localhost:27017/';
 
 const connectToMongo = () =>{
     mongoose.connect(mongoURI, ()=>{
