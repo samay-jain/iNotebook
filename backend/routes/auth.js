05baec3c571b753ed9999c2732e0c85b571a42fa
Samay Jain <samayjain666@gmail.com>
2024-06-15 15:35:00 +0530

    added middleware to decode user using JWT

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,10 +4,11 @@ const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const fetchuser = require('../middleware/fetchuser');
 
 const JWT_SECRET = 'Samayisagood$boy';
 
-// Create a user using: POST "/api/auth/createuser". No login required
+//ROUTE 1: Create a user using: POST "/api/auth/createuser". No login required
 router.post(
   "/createuser",
   [
@@ -66,7 +67,7 @@ router.post(
 
 
 
-//Authenticate a user using: POST "/api/auth/login". No login required
+//ROUTE 2: Authenticate a user using: POST "/api/auth/login". No login required
 router.post(
     "/login",
     [
@@ -113,6 +114,30 @@ router.post(
         }
     }
 );
+
+
+
+
+
+//ROUTE 3: Get loggedin user details using: POST "/api/auth/getuser". login required
+router.post(
+    '/getuser',
+    fetchuser, 
+    async (req, res) => {
+
+
+        try {
+            const userId = req.user.id;
+            const user = await User.findById(userId).select("-password");
+            res.send(user);
+        } 
+        catch (error) {
+            console.error(error.message);
+            res.status(500).send("Internal server error");
+        }
+    }
+);
+
 
 
 
