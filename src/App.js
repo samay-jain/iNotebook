c7dae091f8a8a5c93d6e20517e70775b9354d3d9
Samay Jain <samayjain666@gmail.com>
2024-06-19 16:12:04 +0530

    added login component with API CALL

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import Home from './components/Home';
 import About from './components/About';
 import NoteState from './context/notes/NoteState';
 import Alert from './components/Alert'
+import Login from './components/Login';
+import Signup from './components/Signup';
 
 function App() {
   return (
@@ -25,6 +27,14 @@ function App() {
               
                 <Routes>
                   <Route exact path="/about" element={<About/>}/>
+                </Routes>
+
+                <Routes>
+                  <Route exact path="/login" element={<Login/>}/>
+                </Routes>
+
+                <Routes>
+                  <Route exact path="/signup" element={<Signup/>}/>
                 </Routes>
             </div>
 
---
+++ src/components/Login.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+
+
+const Login = () => {
+
+    const [credentials, setCredentials] = useState({email:"", password:""});
+    const navigate = useNavigate();
+
+
+    const handleSubmit = async(e) =>{
+        //prevent default to prevent reloading of page
+        e.preventDefault();
+
+        //API Call
+        const response = await fetch(`http://localhost:5000/api/auth/login`, {
+            method: "POST", // *GET, POST, PUT, DELETE, etc.
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({email: credentials.email, password: credentials.password})
+          });
+          const json = await response.json();
+          console.log(json);
+
+          if(json.success)
+          {
+            //save auth token and redirect to home page
+            localStorage.setItem('token', json.authtoken);
+            navigate("/");
+          }
+          else
+            alert("Invalid credentials");
+    }
+
+    const onChange = (e) =>{
+        setCredentials({...credentials, [e.target.name]: e.target.value});
+    }
+
+  return (
+    <div>
+        <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+                <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                <input type="email" value={credentials.email} onChange={onChange} className="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+            </div>
+            <div className="mb-3">
+                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                <input type="password" value={credentials.password} onChange={onChange} className="form-control" name="password" id="exampleInputPassword1"/>
+            </div>
+
+            <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+    </div>
+  )
+}
+
+export default Login
